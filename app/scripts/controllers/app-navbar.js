69b293f9f75ffbfa8af823078aad08538eff44e3
Jonathan Lau <jon@jon.sg>
2014-05-17 01:45:54 +0800

    Add app navbar items

---
+++ app/scripts/controllers/app-navbar.js
@@ -0,0 +1,26 @@
+'use strict';
+
+angular.module('feedbakerApp')
+  .controller('AppNavbarCtrl', function ($scope, $location, Auth) {
+    $scope.menu = [{
+      'title': 'Dashboard',
+      'link': '/app/dashboard'
+    }, {
+      'title': 'My Polls',
+      'link': '/app/polls'
+    }, {
+      'title': 'Profile',
+      'link': '/app/profile'
+    }];
+    
+    $scope.logout = function() {
+      Auth.logout()
+      .then(function() {
+        $location.path('/login');
+      });
+    };
+    
+    $scope.isActive = function(route) {
+      return route === $location.path();
+    };
+  });
