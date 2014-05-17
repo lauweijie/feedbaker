3c78f3b78de31ed4b7fccd9fc2490444e0f3e09b
Jonathan Lau <jon@jon.sg>
2014-05-17 13:29:48 +0800

    Stop redundant bootstrap scripts from loading

--- Gruntfile.js
+++ Gruntfile.js
@@ -197,7 +197,19 @@ module.exports = function (grunt) {
       app: {
         html: '<%= yeoman.app %>/views/index.html',
         ignorePath: '<%= yeoman.app %>/',
-        exclude: []
+        exclude: [
+          'bower_components/bootstrap-sass-official/vendor/assets/javascripts/bootstrap/affix.js',
+          'bower_components/bootstrap-sass-official/vendor/assets/javascripts/bootstrap/alert.js',
+          'bower_components/bootstrap-sass-official/vendor/assets/javascripts/bootstrap/button.js',
+          'bower_components/bootstrap-sass-official/vendor/assets/javascripts/bootstrap/carousel.js',
+          'bower_components/bootstrap-sass-official/vendor/assets/javascripts/bootstrap/dropdown.js',
+          'bower_components/bootstrap-sass-official/vendor/assets/javascripts/bootstrap/tab.js',
+          'bower_components/bootstrap-sass-official/vendor/assets/javascripts/bootstrap/transition.js',
+          'bower_components/bootstrap-sass-official/vendor/assets/javascripts/bootstrap/scrollspy.js',
+          'bower_components/bootstrap-sass-official/vendor/assets/javascripts/bootstrap/modal.js',
+          'bower_components/bootstrap-sass-official/vendor/assets/javascripts/bootstrap/tooltip.js',
+          'bower_components/bootstrap-sass-official/vendor/assets/javascripts/bootstrap/popover.js'
+        ]
       }
     },
 
