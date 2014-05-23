601ee9f271f4d9273dea77c3bef5f346d84587b4
Jonathan Lau <jon@jon.sg>
2014-05-23 12:57:28 +0800

    Redirect users to my polls page viewing invalid poll

--- app/scripts/controllers/app-polls-view.js
+++ app/scripts/controllers/app-polls-view.js
@@ -4,6 +4,8 @@ angular.module('feedbakerApp')
   .controller('AppPollsViewCtrl', function ($scope, $location, $routeParams, Poll) {
     Poll.get({'id': $routeParams.id}, function(poll) {
       $scope.poll = poll;
+    }, function() {
+      $location.path('/app/polls');
     });
     $scope.answerLocation = $location.absUrl() + '/answer';
   });
