c899af5cdbcc97499085aa11332c7b5ba7cfbfe1
Jonathan Tan <jhow.tan@gmail.com>
2014-05-24 13:40:51 +0800

    Add display of poll results

--- app/scripts/controllers/app-polls-results.js
+++ app/scripts/controllers/app-polls-results.js
@@ -2,8 +2,12 @@
 
 angular.module('feedbakerApp')
   .controller('AppPollsResultsCtrl', function ($scope, $location, $routeParams, Poll) {
-    Poll.getResults({'id': $routeParams.id}, function(poll) {
-      $scope.poll = poll;
+    Poll.getResults({'id': $routeParams.id}, function(res) {
+      $scope.poll = res.poll;
+      $scope.answerCount = [];
+      for(var i = 0; i < res.answerCount.length; i++) {
+        $scope.answerCount[res.answerCount[i]._id] = res.answerCount[i].count;
+      }
     }, function() {
       $location.path('/app/polls');
     });
