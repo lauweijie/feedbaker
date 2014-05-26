c9ba5018befc5465d5ded78875430856b2596116
Jonathan Tan <jhow.tan@gmail.com>
2014-05-26 13:41:35 +0800

    Fix jshint errors and broken delete modules

--- app/scripts/controllers/app-polls-results.js
+++ app/scripts/controllers/app-polls-results.js
@@ -9,16 +9,16 @@ angular.module('feedbakerApp')
         $scope.answerCount[res.answerCount[i]._id] = res.answerCount[i].count;
       }
       $scope.chartData = [['Choice', 'Number of Answers']];
-      for(var i = 0; i < res.poll.choices.length; i++) {
+      for(i = 0; i < res.poll.choices.length; i++) {
         $scope.chartData[i+1] = [res.poll.choices[i], $scope.answerCount[i] || 0];
-      }      
+      }
       $scope.chartObject = {
         'type': 'PieChart',
         'data': $scope.chartData,
         'options': {
           'legend': 'none'
         }
-      }
+      };
     }, function() {
       $location.path('/app/polls');
     });
