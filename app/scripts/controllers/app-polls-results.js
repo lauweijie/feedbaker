b1a8486044e475911e0fc47ac5ae9297d8786ef3
Jonathan Lau <jon@jon.sg>
2014-05-29 02:51:50 +0800

    Improve UI for poll results page

--- app/scripts/controllers/app-polls-results.js
+++ app/scripts/controllers/app-polls-results.js
@@ -2,21 +2,34 @@
 
 angular.module('feedbakerApp')
   .controller('AppPollsResultsCtrl', function ($scope, $location, $routeParams, Poll) {
+    $scope.colors = ['#3366cc', '#dc3912', '#ff9900', '#109618', '#990099', '#0099c6', '#dd4477', '#66aa00'];
+    $scope.toAlpha = function(index) {
+      return String.fromCharCode(index + 65);
+    };
     Poll.getResults({'id': $routeParams.id}, function(res) {
       $scope.poll = res.poll;
       $scope.answerCount = [];
       for(var i = 0; i < res.answerCount.length; i++) {
         $scope.answerCount[res.answerCount[i]._id] = res.answerCount[i].count;
       }
-      $scope.chartData = [['Choice', 'Number of Answers']];
+      $scope.chartData = [['Choice', 'Responses', { role: 'style' }]];
       for(i = 0; i < res.poll.choices.length; i++) {
-        $scope.chartData[i+1] = [res.poll.choices[i], $scope.answerCount[i] || 0];
+        $scope.chartData[i+1] = [$scope.toAlpha(i), $scope.answerCount[i] || 0, $scope.colors[i]];
       }
       $scope.chartObject = {
-        'type': 'PieChart',
+        'type': 'ColumnChart',
         'data': $scope.chartData,
         'options': {
-          'legend': 'none'
+          'legend': 'none',
+          'vAxis': {
+            'gridlines': {'count': 0}
+          },
+          'bar': {
+            'groupWidth': '40'
+          },
+          'chartArea': {
+            'width': '90%'
+          }
         }
       };
     }, function() {
