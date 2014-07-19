21f5cde7f5e19df8eb32d4f42d3333d6f0e3e160
Jonathan Lau <jon@jon.sg>
2014-07-18 23:53:12 -0700

    Remove Dashboard and provision for 'Help' modal for /app/polls
    Signed-off-by: Jonathan Tan <jhow.tan@gmail.com>

--- app/scripts/controllers/app-navbar.js
+++ app/scripts/controllers/app-navbar.js
@@ -3,23 +3,20 @@
 angular.module('feedbakerApp')
   .controller('AppNavbarCtrl', function ($scope, $location, Auth) {
     $scope.menu = [{
-      'title': 'Dashboard',
-      'link': '/app/dashboard'
-    }, {
       'title': 'My Polls',
       'link': '/app/polls'
     }, {
       'title': 'Profile',
       'link': '/app/profile'
     }];
-    
+
     $scope.logout = function() {
       Auth.logout()
       .then(function() {
         $location.path('/');
       });
     };
-    
+
     $scope.isActive = function(route) {
       return route === $location.path();
     };
