0977151f36c5ef44ba44bd69d742a6eac9dce28d
Jonathan Tan <jhow.tan@gmail.com>
2014-07-29 23:55:43 +0800

    Remove redundancies

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -81,7 +81,6 @@ angular.module('feedbakerApp', [
     }]);
   })
   .run(function ($rootScope, $location, Auth) {
-
     // Redirect to login if route requires auth and you're not logged in
     $rootScope.$on('$routeChangeStart', function (event, next) {
       if (next.authenticate && !Auth.isLoggedIn()) {
