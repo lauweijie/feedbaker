f670fa0ab4db5fd6df3723d3f122786dd8b1d4f9
Jonathan Lau <jon@jon.sg>
2014-09-12 00:03:03 +0800

    Add support for fbkr.me shorturl

--- app/scripts/controllers/app-polls-view.js
+++ app/scripts/controllers/app-polls-view.js
@@ -4,7 +4,7 @@ angular.module('feedbakerApp')
   .controller('AppPollsViewCtrl', function ($scope, $location, $routeParams, Poll) {
     Poll.get({'id': $routeParams.id}, function(poll) {
       $scope.poll = poll;
-      $scope.answerLocation = $location.absUrl().replace($location.url(), '') + '/a/' + poll.shortId;
+      $scope.answerLocation = 'http://fbkr.me/' + poll.shortId;
     }, function() {
       $location.path('/app/polls');
     });
